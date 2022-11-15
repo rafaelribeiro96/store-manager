4212d9befbee0632e26a03891c95b9393776aff1
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-11-15 12:26:56 -0300

    requisito 4 realizado

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productController = require('../controllers/product.controller');
+const validateName = require('../middlewares/validateName');
 
 const router = express.Router();
 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.post('/', productController.createProduct);
+router.post('/', validateName, productController.createProduct);
 
 module.exports = router;
